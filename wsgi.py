cb695eee587226f793b361dce363f90d02aef1fd
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2016-11-07 08:02:25 -0600

    update settings base

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tilkedge.settings.beta")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tikedge.settings.beta")
 
 application = get_wsgi_application()

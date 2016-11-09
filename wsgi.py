bf39bba43ca1d0ccc84e5b53970fa9ef434a04c4
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2016-11-08 18:18:01 -0600

    updated sys path

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR+'/tikedge/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tikedge.settings.beta")
 
 application = get_wsgi_application()
